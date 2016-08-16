d0a2bb7f8972b745e96399a19fd955a644ad10fa
Eric <ericgflynn@gmail.com>
2016-08-16 08:44:56 -0400

    still not working

---
+++ calc.js
@@ -0,0 +1,79 @@
+
+function clearDisplay() {
+    let display = document.getElementById('display');
+    display.value = 0;
+    storedNum = '';
+    calculationFinished = true;
+    operation = operations.none;
+}
+let el = document.getElementById('overlayBtn');
+if(el){
+  el.addEventListener('click', swapper, false);
+}
+let  EinBtn = document.getElementById('1');
+if(EinBtn){
+    EinBtn.addEventListener('click', one);
+}
+ let ZweiBtn = document.getElementById('2');
+ 
+if(ZweiBtn){
+  ZweiBtn.addEventListener('click', two);
+}
+let  DreiBtn = document.getElementById('3');
+if(DreiBtn){
+    DreiBtn.addEventListener('click', 3);
+}
+    
+ let  VierBtn = document.getElementById('4');
+    if(VierBtn){
+    VierBtn.addEventListener('click', 4);
+}
+ let  FunfBtn = document.getElementById('5');
+    if(FunfBtn){
+    FunfBtn.addEventListener('click', 5);
+}
+ let  SechsBtn = document.getElementById('6');
+    if(SechsBtn){
+    SechsBtn.addEventListener('click', 6);
+};
+let  SiebenBtn = document.getElementById('7');
+    if(SiebenBtn){
+    EinBtn.addEventListener('click', 7);
+}
+ let  AchtBtn = document.getElementById('8');
+    if(AchtBtn){
+    AchtBtn.addEventListener('click', 8);
+}
+ let  NeunBtn = document.getElementById('9');
+    if(NeunBtn){
+    NeunBtn.addEventListener('click', 9);
+}
+ let  NullBtn = document.getElementById('0');
+    if(NullBtn){
+    NullBtn.addEventListener('click', 0);
+}
+ let  ClearBtn = document.getElementById('CL');
+    if(ClearBtn){
+    ClearBtn.addEventListener('click', '');
+}
+ let  AddBtn = document.getElementById('add');
+    if(AddBtn){
+    AddBtn.addEventListener('click', '+');
+}
+ let  SubtractBtn = document.getElementById('-');
+    if(SubtractBtn){
+    SubtractBtn.addEventListener('click', '-');
+}
+    let  MultiplyBtn = document.getElementById('*');
+    if(MultiplyBtn){
+    MultiplyBtn.addEventListener('click', x );
+}
+
+let  DivideBtn = document.getElementById('/');
+    if(DivideBtn){
+    DivideBtn.addEventListener('click', '/');
+}
+    let  DecimalBtn = document.getElementById('.');
+    if(DecimalBtn){
+    DecimalBtn.addEventListener('click', '.');
+}

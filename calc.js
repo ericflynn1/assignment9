8323f1fc54e9e86035046b2a846d366e937a9ea2
ericflynn1 <ericgflynn@gmail.com>
2016-09-23 15:05:13 -0400

    working

--- calc.js
+++ calc.js
@@ -1,99 +1,43 @@
-
-// function clearDisplay() {
-//     let display = document.getElementById('display');
-//     display.value = 0;
-//     storedNum = '';
-//     calculationFinished = true;
-//     operation = operations.none;
-// }
-// let el = document.getElementById('overlayBtn');
-// if(el){
-//   el.addEventListener('click', false);
-// }
-// let  EinBtn = document.getElementById('1');
-// if(EinBtn){
-//     EinBtn.addEventListener('click', 1);
-// }
-//  let ZweiBtn = document.getElementById('2');
- 
-// if(ZweiBtn){
-//   ZweiBtn.addEventListener('click', 2);
-// }
-// let  DreiBtn = document.getElementById('3');
-// if(DreiBtn){
-//     DreiBtn.addEventListener('click', 3);
-// }
-    
-//  let  VierBtn = document.getElementById('4');
-//     if(VierBtn){
-//     VierBtn.addEventListener('click', 4);
-// }
-//  let  FunfBtn = document.getElementById('5');
-//     if(FunfBtn){
-//     FunfBtn.addEventListener('click', 5);
-// }
-//  let  SechsBtn = document.getElementById('6');
-//     if(SechsBtn){
-//     SechsBtn.addEventListener('click', 6);
+// Add the two numbers from the DOM
+// function addition(first, second) {
+//     return first + second;
 // };
-// let  SiebenBtn = document.getElementById('7');
-//     if(SiebenBtn){
-//     EinBtn.addEventListener('click', 7);
-// }
-//  let  AchtBtn = document.getElementById('8');
-//     if(AchtBtn){
-//     AchtBtn.addEventListener('click', 8);
-// }
-//  let  NeunBtn = document.getElementById('9');
-//     if(NeunBtn){
-//     NeunBtn.addEventListener('click', 9);
-// }
-//  let  NullBtn = document.getElementById('0');
-//     if(NullBtn){
-//     NullBtn.addEventListener('click', 0);
-// }
-//  let  ClearBtn = document.getElementById('CL');
-//     if(ClearBtn){
-//     ClearBtn.addEventListener('click', '');
-// }
-//  let  AddBtn = document.getElementById('add');
-//     if(AddBtn){
-//     AddBtn.addEventListener('click', '+');
-// }
-//  let  SubtractBtn = document.getElementById('-');
-//     if(SubtractBtn){
-//     SubtractBtn.addEventListener('click', '-');
-// }
-//     let  MultiplyBtn = document.getElementById('*');
-//     if(MultiplyBtn){
-//     MultiplyBtn.addEventListener('click', x );
+// function multiply(first, second) {
+//     return first * second;
 // }
 
-// let  DivideBtn = document.getElementById('/');
-//     if(DivideBtn){
-//     DivideBtn.addEventListener('click', '/');
-// }
-//     let  DecimalBtn = document.getElementById('.');
-//     if(DecimalBtn){
-//     DecimalBtn.addEventListener('click', '.');
+// function subtract(first, second) {
+//     return first - second;
 // }
 
+// Run some code when the page loads.
+window.addEventListener('load', function () {
+    let first = document.getElementById('firstVal');
+    let second = document.getElementById('secondVal');
+    let answer = document.getElementById('answer');
 
-
-function addition() {
-
-}
-
-
-window.addEventListener('load', function (){
-    let first = document.getelementById ('firstVal')
-    let second = document.getelementById ('secondVal');
-    let answer = document.getelementById ('answer');
-    
-    let plusBtn = document.getElementById('addition');
-    plusBtn.addEventListener('click', function()) {
-        console.log('good day');
+    let plusBtn = document.getElementById('plus');
+    plusBtn.addEventListener('click', function () {
         let sum = parseInt(first.value) + parseInt(second.value);
-        answer.textContent = 'Answer' + sum;
-}
-}
+        answer.textContent = 'Answer: ' + sum;
+    });
+    let subtractBtn = document.getElementById('sub');
+    subtractBtn.addEventListener('click', function () {
+        let sum = parseInt(first.value) - parseInt(second.value);
+        answer.textContent = 'Answer: ' + sum;
+    });
+    let multBtn = document.getElementById('mult');
+    multBtn.addEventListener('click', function () {
+        let equal = parseInt(first.value) * parseInt(second.value);
+        answer.textContent = 'Answer: ' + equal;
+    });
+    let divBtn = document.getElementById('div');
+    divBtn.addEventListener('click', function () {
+        let dividend = parseInt(first.value) / parseInt(second.value);
+        answer.textContent = 'Answer: ' + dividend;
+    });
+    let clearBtn = document.getElementById('clear');
+    clearBtn.addEventListener('click', function () {
+        answer.textContent = 'Answer: ', first.value = 0, second.value = 0;
+    });
+});
